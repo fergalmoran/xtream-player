f9ab321dc190d25247e880445f3230ea045ce96a
Fergal Moran <fergal.moran@gmail.com>
2022-04-06 12:45:36 +0100

    Theming added

---
+++ frontend/src/context/index.ts
@@ -0,0 +1,4 @@
+import { SidebarContext } from "./sidebar.context";
+import { ThemeContext } from "./theme.context";
+
+export { SidebarContext, ThemeContext };
---
+++ frontend/src/context/sidebar.context.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface ISidebarProvider {
+  children: React.ReactChild;
+}
+interface ISidebarProviderContext {
+  isSidebarOpen: boolean;
+  toggleSidebar: () => void;
+  closeSidebar: () => void;
+}
+export const SidebarContext = React.createContext<ISidebarProviderContext>({
+  isSidebarOpen: true,
+  toggleSidebar: () => {},
+  closeSidebar: () => {},
+});
+
+export const SidebarProvider = ({ children }: ISidebarProvider) => {
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const value = React.useMemo(
+    () => ({
+      isSidebarOpen,
+      toggleSidebar,
+      closeSidebar,
+    }),
+    [isSidebarOpen]
+  );
+
+  return (
+    <SidebarContext.Provider value={value}>{children}</SidebarContext.Provider>
+  );
+};
